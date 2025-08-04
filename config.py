88106188930b552ce2c359d355dea4b301d92340
zhaoxu98 <zhaohsu98@gmail.com>
2025-08-04 17:40:41 +0800

    test

--- config.py
+++ config.py
@@ -2,5 +2,5 @@
 # Set your start date here (optional)
 # Format: YYYY-MM-DD
 
-START_DATE = "2025-07-01"  # Change this to your actual start date
+START_DATE = "2025-08-01"  # Change this to your actual start date
 # START_DATE = None  # Uncomment this line if you don't want day counting 
